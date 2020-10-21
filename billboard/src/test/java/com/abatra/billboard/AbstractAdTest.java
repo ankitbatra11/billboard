517b277cb0625d217f9a78e81cecf14ef3fb9fc0
Ankit Batra <ankit.batra11@gmail.com>
2020-10-21 13:20:39 +0530

    add untracked files

---
+++ billboard/src/test/java/com/abatra/billboard/AbstractAdTest.java
@@ -0,0 +1,14 @@
+package com.abatra.billboard;
+
+import org.junit.Test;
+
+public class AbstractAdTest {
+
+    @Test
+    public void testAlreadyLoadedTestIsNotLoadedAgain() {
+    }
+
+    @Test
+    public void testAlreadyLoadingAdIsNotLoadedAgain() {
+    }
+}
