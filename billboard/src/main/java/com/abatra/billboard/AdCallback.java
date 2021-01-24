931a2780eaeff46ba32dc695a127640952d54f62
Ankit Batra <ankit.batra11@gmail.com>
2021-01-24 21:20:06 +0530

    added full screen content callback adapter for AdCallback

--- billboard/src/main/java/com/abatra/billboard/AdCallback.java
+++ billboard/src/main/java/com/abatra/billboard/AdCallback.java
@@ -1,5 +1,8 @@
 package com.abatra.billboard;
 
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.FullScreenContentCallback;
+
 public interface AdCallback {
 
     AdCallback NO_OP = new AdCallback() {
@@ -20,5 +23,37 @@ public interface AdCallback {
     default void adDisplayed() {
     }
 
-    default void adFailedToShow() {}
+    default void adFailedToShow() {
+    }
+
+    default FullScreenContentCallback asFullScreenContentCallback() {
+        return new FullScreenContentCallbackAdapter(this);
+    }
+
+    class FullScreenContentCallbackAdapter extends FullScreenContentCallback {
+
+        private final AdCallback adCallback;
+
+        public FullScreenContentCallbackAdapter(AdCallback adCallback) {
+            this.adCallback = adCallback;
+        }
+
+        @Override
+        public void onAdFailedToShowFullScreenContent(AdError adError) {
+            super.onAdFailedToShowFullScreenContent(adError);
+            adCallback.adFailedToShow();
+        }
+
+        @Override
+        public void onAdShowedFullScreenContent() {
+            super.onAdShowedFullScreenContent();
+            adCallback.adDisplayed();
+        }
+
+        @Override
+        public void onAdDismissedFullScreenContent() {
+            super.onAdDismissedFullScreenContent();
+            adCallback.adClosed();
+        }
+    }
 }
